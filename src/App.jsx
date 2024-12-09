0a4ba67f9e75bb9f7b6545e8d55261857dee7e6d
staceylouis <30754595+StaceyLouis@users.noreply.github.com>
2024-12-09 20:24:50 +0000

    connected to the database

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://todo-backend-4fyf.onrender.com/todos/")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  });
+
+  console.log(data);
 
   return (
-   <>
-   tasking
-   </>
-  )
+    <div>
+      {data.map((i) => (
+        <p key={i.id}>{i.title}</p>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
